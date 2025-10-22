d808ac92704e1a3a4faf7989da1fe3174ca03223
Partha <80641510+parthadeori@users.noreply.github.com>
2025-10-22 23:06:06 +0530

    Remove comments and clean up main.py

--- Problem 1 (Easy)/main.py
+++ Problem 1 (Easy)/main.py
@@ -1,16 +1,3 @@
-# Step 1: Define a function and create a dictionary
-
-# Step 2: Loop through each number using enumerate()
-
-# Step 3: Find the complement
-
-# Step 4: Check if complement exists
-
-# Step 5: Add the current number to the dictionary
-
-# [2, 7, 11, 15]
-# target = 9
-
 def two_sum(nums, target):
     num_to_index = {}
 
@@ -24,4 +11,3 @@ def two_sum(nums, target):
 nums =[7, 3, 2, 5]
 target = 8
 print(two_sum(nums, target))
-        
